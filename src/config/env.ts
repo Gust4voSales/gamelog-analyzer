2f696debd4296bae8b78388abbbdcff1695e219a
Gust4voSales <manoel0gustavo@gmail.com>
2025-08-12 19:29:45 -0300

    chore: start nestjs project

---
+++ src/config/env.ts
@@ -0,0 +1,3 @@
+export const EnvConfig = {
+  port: process.env.PORT || '3333',
+};
---
+++ src/main.ts
@@ -0,0 +1,11 @@
+import { NestFactory } from '@nestjs/core';
+import { AppModule } from './app.module';
+import { EnvConfig } from './config/env';
+
+async function bootstrap() {
+  const app = await NestFactory.create(AppModule);
+  await app.listen(EnvConfig.port, () => {
+    console.log(`Server is running on port ${EnvConfig.port}`);
+  });
+}
+bootstrap();
